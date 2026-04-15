379db54141e1c71ee9eaec44b63ae0b9fc4f2b2c
HmmInterestingHmm <flopezjr2025@gmail.com>
2026-04-14 21:11:03 -0500

    Modified File: Removed the returns

--- television.py
+++ television.py
@@ -16,8 +16,6 @@ class Television:
         else:
             self.__status = True
 
-        return self.__status
-
     def mute(self):
         if self.__status:
             if self.__muted:
@@ -25,27 +23,17 @@ class Television:
             else:
                 self.__muted = True
 
-        return self.__muted
-
     def channel_up(self):
-        if not self.__status:
-            return self.__channel
-
-        self.__channel += 1
-        if self.__channel > Television.MAX_CHANNEL:
-            self.__channel = Television.MIN_CHANNEL
-
-        return self.__channel
+        if self.__status:
+            self.__channel += 1
+            if self.__channel > Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
-        if not self.__status:
-            return self.__channel
-
-        self.__channel -= 1
-        if self.__channel < Television.MIN_CHANNEL:
-            self.__channel = Television.MAX_CHANNEL
-
-        return self.__channel
+        if self.__status:
+            self.__channel -= 1
+            if self.__channel < Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
         if self.__status:
@@ -55,9 +43,6 @@ class Television:
             if self.__volume > Television.MAX_VOLUME:
                 self.__volume = Television.MAX_VOLUME
 
-
-        return self.__volume
-
     def volume_down(self):
         if self.__status:
             if self.__muted:
@@ -66,11 +51,7 @@ class Television:
             if self.__volume < Television.MIN_VOLUME:
                 self.__volume = Television.MIN_VOLUME
 
-
-        return self.__volume
-
     def __str__(self):
         if self.__muted:
             return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [0]"
-        else:
-            return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]"
+        return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]"
